24bd87fbd03c811d7a9e5e959b88a639c42094d4
thybui1903 <thybui1903qn@gmail.com>
2025-06-03 22:33:31 +0700

    new file:   data/labeled/labeled_data.json #	new file:   notebooks/tempCodeRunnerFile.ipynb #	modified:   requirements.txt #	modified:   src/config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 RAW_DATA_PATH = r"D:\Tiki_ABSA\data\raw\raw_data.json"
-PROCESSED_DATA_PATH = r"D:\Tiki_ABSA\data\processed\processed_data.json"
 MAX_REVIEWS_PER_PRODUCT = 6000
+
+PROCESSED_DATA_PATH = r"D:\Tiki_ABSA\data\processed\processed_data.json"
+LABELED_DATA_PATH = r"D:\Tiki_ABSA\data\labeled\labeled_data.json"
