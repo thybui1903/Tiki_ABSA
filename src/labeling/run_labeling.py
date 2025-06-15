f87ab7e0c29eb9f0b410aef70beb9a009c0650a2
thybui1903 <thybui1903qn@gmail.com>
2025-06-15 21:04:13 +0700

    # Changes to be committed: #	deleted:    scripts/__pycache__/run_crawling.cpython-313.pyc #	deleted:    scripts/__pycache__/run_preprocessing.cpython-313.pyc #	deleted:    scripts/run_experiment.py #	renamed:    scripts/run_crawling.py -> src/crawl/run_crawling.py #	renamed:    scripts/run_labeling.py -> src/labeling/run_labeling.py #	renamed:    scripts/run_preprocessing.py -> src/preprocessing/run_preprocessing.py

--- src/labeling/run_labeling.py
+++ src/labeling/run_labeling.py
@@ -2,8 +2,8 @@ from src.labeling.batch_labeling import process_reviews_by_batch
 
 if __name__ == "__main__":
     process_reviews_by_batch(
-        input_file='data/processed/processed_data_from_3175.json',
-        output_file='data/labeled/labeled_data_from_3175.json',
+        input_file='data/processed/processed_data.json',
+        output_file='data/labeled/labeled_data.json',
         batch_size=3,
         sleep_time=2
     )
